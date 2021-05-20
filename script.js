a002433c454dc2575579798dde39ecd1aaa00124
Paige Godfrey <paagodfrey@gmail.com>
2021-05-20 12:30:55 -0700

    created roadmap

---
+++ script.js
@@ -0,0 +1,40 @@
+/* ** STEPS **
+   
+  HTML
+    Setup HTML elements
+      Navigation
+        SearchBar
+      MovieList
+        Movie
+        MovieDetail
+      LoadMoreButton
+
+  JS
+    Define variables for selected elements
+    Get API key
+    Test API calls
+      All current movies
+      Search results
+    Get movies displayed
+    
+
+  CSS
+
+
+  */
+
+getCurrentMovies
+
+loadMoreMovies
+
+getSearchResults
+
+handleSearch
+
+displayMovies
+
+displayMovieDetails
+
+closeMovieDetails
+
+init

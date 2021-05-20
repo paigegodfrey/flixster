d6655c45a3f2500f1aeeecab6caece811e7ecd5f
Paige Godfrey <paagodfrey@gmail.com>
2021-05-20 12:50:50 -0700

    SearchButton built and styled

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-/* ** STEPS **
-   
-  HTML
-    Setup HTML elements
-      Navigation
-        SearchBar
-      MovieList
-        Movie
-        MovieDetail
-      LoadMoreButton
 
-  JS
-    Define variables for selected elements
-    Get API key
-    Test API calls
-      All current movies
-      Search results
-    Get movies displayed
-    
-
-  CSS
-
-
-  */
 
 getCurrentMovies
 

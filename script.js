6513e72d43553ce4e60617e41c2cb5df67117104
Paige Godfrey <paagodfrey@gmail.com>
2021-05-20 18:00:23 -0700

    LoadMoreButton show/hide working

--- script.js
+++ script.js
@@ -1,68 +1,69 @@
 const API_KEY = "67cbdd034db91fb68a728ea0c24c5dd3";
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
+const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
-const searchBtn = document.getElementById('search-btn');
 const movieList = document.getElementById('movie-list');
-const loadMoreBtn = document.getElementById('load-more-movies-btn');
+const notFoundMsg = document.getElementById('not-found-msg');
+const loadMoreMoviesContainer = document.querySelector('.load-more-movies-container')
+const loadMoreMoviesBtn = document.getElementById('load-more-movies-btn');
 
 let moviePage = 1;
-
-
-// helper function to check for empty string
-const isEmpty = str => {
-  return (!str || str.length === 0);
-}
 
 // helper function to check for blank string
 const isBlank = str => {
   return (!str || /^\s*$/.test(str));
 }
 
-// Make API call and return movies now playing as HTML
+// Makes conditional API call and returns movies as HTML 
 const getMovies = async searchTerm => {
-  let moviesRes;
-
-  if (isEmpty(searchTerm) || isBlank(searchTerm)) {
-    moviesRes = await fetchAllMovies();
-  } else {
-    moviesRes = await fetchSearchMovies(searchTerm);
-  }
-
+  let moviesRes = isBlank(searchTerm) ? await fetchMoviesNowPlaying() : await fetchSearchMovies(searchTerm);
+  !isBlank(searchTerm) ? loadMoreMoviesContainer.classList.add('hidden') : loadMoreMoviesContainer.classList.remove('hidden');
+  
   displayMovies(moviesRes.results);
 }
 
-
-const fetchAllMovies = async () => {
+// Makes API call for movies now playing and returns data as JSON
+const fetchMoviesNowPlaying = async () => {
   // Found in API documentation Movies -> GET Now Playing
   let allMoviesRes = await (await fetch(`${API_BASE_URL}/movie/now_playing?api_key=${API_KEY}&page=${moviePage}`)).json();
   return allMoviesRes;
 }
 
-// Make API call and return movies now playing based on search term
+// Makes API call for movies based on search term and returns data as JSON
 const fetchSearchMovies = async searchTerm => {
   // Found in API documentation Getting Started -> Search & Query
   let searchMoviesRes = await (await fetch(`${API_BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchTerm}`)).json();
   return searchMoviesRes;
 }
 
-// Event handler for SearchBar
-const handleSearch = () => {
-  
+// Event handler for search form submit
+const handleSearchFormSubmit = evt => {
+  evt.preventDefault();
+  let searchTerm = searchInput.value;
+  getMovies(searchTerm);
 }
 
 // Return movies now playing as HTML
 const displayMovies = movieData => {
+  // handle empty results
+  if (!movieData.length) {
+    loadMoreMoviesContainer.classList.add('hidden');
+    notFoundMsg.innerText = 'Sorry, no results were found';
+    movieList.innerHTML = '';
+    return;
+  }
+
   let moviesHTML = movieData.map(movie => `
-  <li class="movie-card">
+    <li class="movie-card">
       <img class="movie-poster" src=${movie.poster_path ? `https://image.tmdb.org/t/p/w342/${movie.poster_path}` : 'default_poster_img.jpg'} alt="${movie.title}"/>
       <div class="movie-details">
         <h3 class="movie-title">${movie.title}</h3>
         <div class="movie-votes">⭐ ${movie.vote_average}</div>
       </div>
-  </li>
-`).join('');
-
+    </li>
+  `).join('');
+  notFoundMsg.innerText = '';
   movieList.innerHTML = moviesHTML;
 }
 
@@ -72,4 +73,4 @@ const displayMovies = movieData => {
 
 // loadMoreMovies
 
-// init
+searchForm.addEventListener('submit', handleSearchFormSubmit);
